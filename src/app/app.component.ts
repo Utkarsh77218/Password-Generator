2e300bb16a66119fff07512332365fc7714af7df
Utkarsh77218 <saxena2002utkarsh@gmail.com>
2025-01-02 17:34:33 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswordGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'password-generator';
+  
 }
---
+++ src/app/password-generator/password-generator.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-password-generator',
+  templateUrl: './password-generator.component.html',
+  styleUrl: './password-generator.component.css',
+  imports: [FormsModule],
+  standalone: true,
+})
+export class PasswordGeneratorComponent {
+  passwordLength: number = 8;
+  generatedPassword: string = '';
+  includeAlphabets: boolean = true;
+  includeNumbers: boolean = false;
+  includeSpecialCharacters: boolean = false;
+
+  generatePassword(): void {
+    let characters = '';
+    let password = '';
+
+    if (this.includeAlphabets) {
+      characters += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+
+    if (this.includeNumbers) {
+      characters += '1234567890';
+    }
+
+    if (this.includeSpecialCharacters) {
+      characters += '!@#$%^&*()_+~`|}{[]:;?><,./';
+    }
+    
+    for (let i = 0; i < this.passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+
+    this.generatedPassword = password;
+  }
+}
