9c8ad4d644e2a3316a28e30b7944ea86f07fc0b1
Utkarsh77218 <saxena2002utkarsh@gmail.com>
2025-01-03 19:44:44 +0530

    add hamburger menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    menuOpen: boolean = false;
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    }
 }
